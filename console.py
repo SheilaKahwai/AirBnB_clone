354fbdf92e41de58c04c1eccf076589635ecbce0
Sheila Kahwai <kahwaisheila@gmail.com>
2022-10-26 08:55:59 -0700

    override emptyline method

--- console.py
+++ console.py
@@ -1,26 +1,31 @@
-from cmd import Cmd
-
-
+#!/usr/bin/python3
 """
 The Console Module
 Serves as the entry point of our command interpreter.
 """
 
 
-class HbnbConsole(Cmd):
-	"""Our subclass of the Cmd class"""
-	prompt = '(hbnb) '
+import cmd
+
+
+class HbnbConsole(cmd.Cmd):
+    """Our subclass of the Cmd class"""
+    prompt = '(hbnb) '
 	
-	def do_quit(self, arg):
-		"""Exits the shell"""
-		self.close()
-		return True
+    def do_quit(self, arg):
+        """Exits the shell"""
+        return True
 
-	def help_quit(self):
-		print("Exits the shell")
+    def help_quit(self):
+        print("Exits the shell")
 
-	do_EOF = do_quit
-	help_EOF = help_quit
+    def emptyline(self):
+        """Overrides the emptyline method"""
+        pass
+
+    do_EOF = do_quit
+    help_EOF = help_quit
 
 if __name__ == '__main__':
-    	HbnbConsole().cmdloop()
+    HbnbConsole().cmdloop()
+    print()
